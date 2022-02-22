db75fdf4c15d4a7d27333bf81c5280e5cfe6f6ba
Dylan Sabuda <dylanscoding@gmail.com>
2022-02-22 13:37:47 -0500

    Added gasLevelAlert

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,9 +1,13 @@
 import random
-
 # Gas Level Function
 def gasLevelGauge():
-    gasLevelList = ["Empty Tank", "Quarter Tank", "Half Tank", "3 Quarters Tank", "Full Tank"]
+    gasLevelList = ["Empty Tank", "Low Tank", "Quarter Tank", "Half Tank", "3 Quarters Tank", "Full Tank"]
     currentGasLevel = random.choice(gasLevelList)
-    print(currentGasLevel)
+    return currentGasLevel
 
-gasLevelGauge()
+# Create If-Elif-Else statements using Comparative Operators to display gas level messages
+def gasLevelAlert():
+    if gasLevelGauge() == "Empty Tank":
+        print("\033[1;31m  ***WARNING***\nGas tank is empty. \nCalling emergency contact(s)")
+
+gasLevelAlert()
