89c16b698de258dcb505ba11ceaab7fd609cc04a
Dylan Sabuda <dylanscoding@gmail.com>
2022-02-22 13:21:01 -0500

    Added sleep

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,4 +1,9 @@
 # Codename Hornet
+# https://realpython.com/python-sleep/#adding-a-python-sleep-call-with-timesleep
+
+from time import sleep  # imports the sleep function from time
+
 
 print("Welcome to Hornet's Info Tech Center!\n")
+sleep(1)
 print("Hornet's Operating System Booting Up!")
